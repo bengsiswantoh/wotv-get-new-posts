20b9d1186117eb6ad5813583fd67ed3144893ae1
Bengsiswanto <bengsiswantoh@gmail.com>
2021-02-17 13:50:26 +0700

    add images

--- get_news.js
+++ get_news.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const debug = true;
+const debug = false;
 const maxLength = 2000;
 const urlRoot = 'https://site.na.wotvffbe.com';
 const urlList =
@@ -15,13 +15,7 @@ const dataFilename = 'data.json';
 let rawData = fs.readFileSync(dataFilename);
 let dataFile = JSON.parse(rawData);
 
-const createContent = (contents) => {
-  const content = contents.join('\n\n');
-  return content;
-};
-
-const sendMessage = async (contents) => {
-  const content = createContent(contents);
+const sendMessage = async (content) => {
   if (debug) {
     console.log('===');
     console.log(content);
@@ -44,21 +38,20 @@ const getDetails = async (url) => {
 
   let selector = cheerio.load(data);
   const articleBody = selector('.article_body').html();
-
   selector = cheerio.load(articleBody);
+
   const images = selector('img');
 
   let result = '';
   images.each(async function (index, e) {
-    const src = selector(this).get(0).attribs['src'];
+    const src = selector(this).attr('src');
     result = `${result}${urlRoot}${src}\n`;
   });
 
-  const dives = selector('div');
-  dives.each(async function (index, e) {
-    const text = selector(this).text();
-    // console.log(text);
-  });
+  // const dives = selector('div');
+  // dives.each(async function (index, e) {
+  //   const text = selector(this).text();
+  // });
 
   return result;
 };
@@ -79,7 +72,7 @@ const main = async (dataFile) => {
     var found = false;
 
     items.each(async function (index, e) {
-      const id = selector(this).get(0).attribs['data-tab'];
+      const id = selector(this).data('tab');
 
       // save latest id
       if (index === 0) {
@@ -96,29 +89,22 @@ const main = async (dataFile) => {
         const info = selector(this).find('p');
 
         const url = urlDetail.replace('{id}', id);
-        // const details = await getDetails(url);
-
-        const newContent = `${info.text()}\n${url}`;
-        // const newContent = `${info.text()}\n${url}\n${details}`;
-
-        contents.push(newContent);
-
-        let content = createContent(contents);
-        if (content.length > maxLength) {
-          contents.pop();
-          await sendMessage(contents);
-
-          contents = [newContent];
-        }
+        const content = { url, message: `${info.text()}\n${url}\n` };
+        contents.push(content);
       }
     });
+
+    for (const content of contents) {
+      const details = await getDetails(content.url);
+      await sendMessage(content.message + details);
+    }
 
     // if new content/s found
     if (contents.length > 0) {
       dataFile.last_id = dataFile.new_id;
       delete dataFile.new_id;
 
-      await sendMessage(contents);
+      // await sendMessage(contents);
 
       fs.writeFileSync(dataFilename, JSON.stringify(dataFile));
     }
